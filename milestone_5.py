6126c0aa724379f93ab649e99201daa2bf5db53e
Holfire7 <167590185+Holfire7@users.noreply.github.com>
2024-06-17 01:45:22 +0100

    Update milestone_5.py

---
+++ milestone_5.py
@@ -0,0 +1,55 @@
+import random
+
+from milestone_4 import game
+
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.word = random.choice(self.word_list).lower()
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for index, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[index] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word. You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Enter a single letter: ").lower()
+            if not (guess.isalpha() and len(guess) == 1):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+
+# Create an instance of Hangman with a word list
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+        elif game.num_letters == 0:
+           print("congratulations. You won the game!")
+           break
+        else:
+            game.ask_for_input()
+            print(f"word: {' '.join(game.word_guessed)}")
+
+word_list = ['mango', 'banana', 'watermelon', 'pineapple', 'guava']
+play_game(word_list)
